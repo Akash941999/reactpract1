a4dc79e3925a9805e1661cf0525d714521fc96e7
Akash Kumar <akash.kumar@unthinkable.co>
2021-12-12 09:22:41 +0530

    first commit

---
+++ src/components/Test/test.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function profile(prop){
+
+    return(
+        <div>
+            <h1>Hii I am from outside</h1>
+            <h1>{prop.text.name}</h1>
+            <h1>{prop.text.name}</h1>
+
+        </div>
+    )
+}
